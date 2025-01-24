ee7a624c2d474ebcec5f4a9b9385b2258d560d25
herrera-alison <herrera_alison@bah.com>
2025-01-24 15:22:24 -0500

    Removes commented code from index

--- index.ts
+++ index.ts
@@ -1,24 +1,3 @@
-// import { EC2Client, DescribeImagesCommand } from "@aws-sdk/client-ec2";
-// import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
-
-// export async function getAwsMachineImages() {
-//   const client = new EC2Client({
-//     credentials: fromNodeProviderChain({ profile: "default" }),
-//     // clientConfig: { region }, // This line is the fix and is therefore commented out for this reproduction
-//   });
-
-//   try {
-//     const command = new DescribeImagesCommand({});
-//     const response = await client.send(command);
-//     console.log(response);
-//   } catch (error) {
-//     console.error("Error retrieving AWS machine images:", error);
-//     throw error;
-//   }
-// }
-
-// getAwsMachineImages();
-
 import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts';
 
